d932e235610748af9a42f4f4d5f03c4ffe8dc2b5
Haitam Elhanafi <haitamelhanafi@gmail.com>
2025-04-30 16:51:55 +0100

    app.py

--- app.py
+++ app.py
@@ -743,4 +743,6 @@ with gr.Blocks(title="Gestionnaire d'Inventaire E-commerce", css="""
 
 # Launch the app
 if __name__ == "__main__":
-    app.launch()
+    import os
+    port = int(os.environ.get("PORT", 7860))
+    app.launch(server_name="0.0.0.0", server_port=port)
